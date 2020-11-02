c7deb6cd57b1872bbd8352aabe8cf0c3633edb71
Mariya Burdo <62555174+mashaBurdo@users.noreply.github.com>
2020-11-02 11:11:16 +0300

    dev and bot info

--- test_bot.py
+++ test_bot.py
@@ -29,25 +29,6 @@ def word_combination():
 
     return result
 
-def holland_combination():
-    conn = sqlite3.connect('words.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM words WHERE genere !='adj' ORDER BY RANDOM() LIMIT 1")
-    word = cursor.fetchall()
-    #print(word)
-    conn.close()
-
-    noEnd = 'голландск'
-
-    if word[0][2] == 'he':
-        result = noEnd +"ий "+ word[0][1]
-    elif word[0][2] == 'she':
-        result = noEnd +"ая "+ word[0][1]
-    elif word[0][2] == 'it':
-        result = noEnd +"ое "+ word[0][1]
-
-    return result
-
 
 decide = ['Да', 'Нет', 'Наверное', 'Духи говорят да', 'Звезды говорят нет', 'Да, если сегодня четное число', 'Нет, если сегоня дождь', '42', 'Ты разольешь кофе', 'Не могу сказать', 'Неопределенно']
 bot_description = '''Бот является тестовой площадкой для моих проектов. На данный момент самое интересное в нем - генератор случайных словосочетаний. В планах создание генератора текста при помощий марковских цепей. Можете написать мне свой отзыв или идею для проета. Мои контакты можно найти в разделе "О создателе".'''
@@ -58,9 +39,8 @@ dev_description = '''Я Мария Бурдо, студентка стомфак
 bot = telebot.TeleBot('1448303289:AAEg0b7k-j3i-4G47J6hqh8q44v16cKxwEY')
 
 keybord1 = telebot.types.ReplyKeyboardMarkup(True)
-keybord1.row('Помоги решить', 'Голландская игра')
+keybord1.row('Помоги решить', 'Брось кубик')
 keybord1.row('Случайное словосочетание')
-keybord1.row('Дата', 'День недели')
 keybord1.row('О создателе', 'О боте')
 
 @bot.message_handler(commands=['start'])
@@ -73,16 +53,14 @@ def send_text(message):
         bot.send_message(message.chat.id, bot_description)
     elif message.text.lower() == 'о создателе':
         bot.send_message(message.chat.id, dev_description)
-    elif message.text.lower() == 'дата':
-        bot.send_message(message.chat.id, datetime.datetime.today().strftime('%d.%m.%Y'))
-    elif message.text.lower() == 'день недели':
-        bot.send_message(message.chat.id, datetime.datetime.today().strftime('%A'))
     elif message.text.lower() == 'помоги решить':
         bot.send_message(message.chat.id, random.choice(decide))
     elif message.text.lower() == 'случайное словосочетание':
         bot.send_message(message.chat.id, word_combination().capitalize())
-    elif message.text.lower() == 'голландская игра':
-        bot.send_message(message.chat.id, holland_combination().capitalize())
+    elif message.text.lower() == 'брось кубик':
+        bot.send_message(message.chat.id, random.randint(0, 7))
+    else:
+        bot.send_message(message.chat.id, 'Я вас не понимаю:(')
 
 
 bot.polling()
